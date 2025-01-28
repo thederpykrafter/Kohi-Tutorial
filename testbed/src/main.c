2ecf530000480e98b4774b67002e180439825aa7
thederpykrafter <thederpykrafter@gmail.com>
2025-01-28 13:49:23 -0500

    logging and assertions

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,14 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-    print_int(42);
+    KFATAL("A test message: %f", 3.24f);
+    KERROR("A test message: %f", 3.24f);
+    KWARN("A test message: %f", 3.24f);
+    KINFO("A test message: %f", 3.24f);
+    KDEBUG("A test message: %f", 3.24f);
+    KTRACE("A test message: %f", 3.24f);
+
+    KASSERT(1 == 0);
     return 0;
 }
