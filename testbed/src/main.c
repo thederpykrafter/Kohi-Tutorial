8b81d12fad525d275206a30bc8410d0abcffaa28
thederpykrafter <thederpykrafter@gmail.com>
2025-01-28 23:02:47 -0500

    windows platform layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+// TODO: test
+#include <platform/platform.h>
+
 int main(void) {
     KFATAL("A test message: %f", 3.24f);
     KERROR("A test message: %f", 3.24f);
@@ -9,6 +12,13 @@ int main(void) {
     KDEBUG("A test message: %f", 3.24f);
     KTRACE("A test message: %f", 3.24f);
 
-    KASSERT(1 == 0);
+    platform_state state;
+    if (platform_startup(&state, "KohiEngineTestbed", 100, 100, 1280, 720)) {
+        while(TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
+    
     return 0;
 }
