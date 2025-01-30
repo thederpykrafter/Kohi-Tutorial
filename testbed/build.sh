48d51a999a0323d2b08020c41d10271d0a001f2f
thederpykrafter <thederpykrafter@gmail.com>
2025-01-29 20:08:21 -0500

    typos

--- testbed/build.sh
+++ testbed/build.sh
@@ -4,19 +4,19 @@ set echo on
 
 mkdir -p ../bin
 
-# Get a list of all the .c files
-cFileNames=$(find . -type f -name "*.c")
+# Get a list of all the .c files.
+cFilenames=$(find . -type f -name "*.c")
 
-# echo "Files:" $cFileNames
+# echo "Files:" $cFilenames
 
 assembly="testbed"
-compilerFlags="-g -fdeclspec -fPIC"
-# -fms-extensions
+compilerFlags="-g -fdeclspec -fPIC" 
+# -fms-extensions 
 # -Wall -Werror
 includeFlags="-Isrc -I../engine/src/"
 linkerFlags="-L../bin/ -lengine -Wl,-rpath,."
 defines="-D_DEBUG -DKIMPORT"
 
-echo "Buileding $assembly..."
-echo clang $cFileNames $compilerFlags -o ../bin/$assembly $defines $includeFlags $linkerFlags
-clang $cFileNames $compilerFlags -o ../bin/$assembly $defines $includeFlags $linkerFlags
+echo "Building $assembly..."
+echo clang $cFilenames $compilerFlags -o ../bin/$assembly $defines $includeFlags $linkerFlags
+clang $cFilenames $compilerFlags -o ../bin/$assembly $defines $includeFlags $linkerFlags
